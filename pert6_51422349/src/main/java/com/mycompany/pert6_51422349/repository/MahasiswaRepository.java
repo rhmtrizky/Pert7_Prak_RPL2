42583433b3c9d0c0927552c4f24b427e5339cd24
Rahmat Rizky Rifai <rahmatrizkyrifai@gmail.com>
2025-12-03 10:17:46 +0700

    First commit

---
+++ pert6_51422349/src/main/java/com/mycompany/pert6_51422349/repository/MahasiswaRepository.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert6_51422349.repository;
+
+import com.mycompany.pert6_51422349.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author rahma
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+
+}
+
+
+
+
